a059a1e7869b29d1ef1b96e41c5cdff4bb65b916
sa50team4 <70044989+sa50team4@users.noreply.github.com>
2020-08-23 01:43:11 +0800

    joe
    -add btn to Save

--- JSONParsingExample/app/src/main/java/iss/workshop/jsonparsingexample/PurchaseOrderCreateWithItems.java
+++ JSONParsingExample/app/src/main/java/iss/workshop/jsonparsingexample/PurchaseOrderCreateWithItems.java
@@ -4,23 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import iss.workshop.jsonparsingexample.Models.PO;
 import iss.workshop.jsonparsingexample.Models.POItems;
+import iss.workshop.jsonparsingexample.Models.PurchaseOrderStatus;
+import iss.workshop.jsonparsingexample.Models.TestDTO;
 
 public class PurchaseOrderCreateWithItems extends AppCompatActivity implements GetItemsListAccordingToSupplierData.OnDataAvailable{
 
     public static final String TAG = "ItemList";
 
-    public String mURL = "http://192.168.68.110/PO/POItemApi";
+    public String mURL = "http://192.119.86.65:90/PO/POItemApi";
     RecyclerView rView;
+    Button mbtnSave;
     PurchaseOrderCreateWithItemAdapter adapter;
+
+    PostJsonData mPostJsonData;
+    private String mPostURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*mbtnSave = findViewById(R.id.btnPOSave);
+        mbtnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPostJsonData = new PostJsonData(PurchaseOrderCreateWithItems.this);
+                mURL = "http://192.119.86.65:90/PO/POSave";
+
+                try {
+                    callAPI();
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
         setContentView(R.layout.activity_purchase_order_create_with_items);
 
         rView = (RecyclerView) findViewById(R.id.supplierCreateWithItemListRecyclerView);
@@ -54,4 +85,51 @@ public class PurchaseOrderCreateWithItems extends AppCompatActivity implements G
 
             Log.d(TAG, "onDataAvailable: ends");
         }
+
+/*
+    public void callAPI() throws JsonProcessingException {
+
+        TestDTO testDTO = new TestDTO();
+        testDTO.setId(30);
+        testDTO.setName("Joe");
+
+        POItems poItems = new POItems();
+        poItems.setSupplierID(1);
+        poItems.setPoStatus(PurchaseOrderStatus.Processing);
+        ObjectMapper mapper = new ObjectMapper();
+        String data = mapper.writeValueAsString(poItems);
+
+        JSONObject demoObject = new JSONObject();
+        try {
+
+            demoObject.put("data", data);
+
+            mPostJsonData.loadJsonData(data);
+            mPostJsonData.execute(mURL);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void postJsonDataOnDownloadComplete(String data, DownloadStatus status) {
+        if(status == DownloadStatus.OK) {
+
+            // work with response data from POST Request API
+            try {
+
+                JSONObject jsonData = new JSONObject(data);
+                JSONObject result = jsonData.getJSONObject("result");
+
+                String text = result.getString("Message");
+                Toast.makeText(getApplicationContext(), text,
+                        Toast.LENGTH_SHORT).show();
+
+            } catch(JSONException jsone) {
+                jsone.printStackTrace();
+                status = DownloadStatus.FAILED_OR_EMPTY;
+            }
+        }
+    }*/
 }
