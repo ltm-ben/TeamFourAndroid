bbcb4e9207a90ea67846c03306186005d4fad005
shermainelim <shermaine.lim.si.hui@gmail.com>
2020-08-24 22:50:11 +0800

    barcha

--- JSONParsingExample/app/src/main/java/iss/workshop/jsonparsingexample/BarChartActivity.java
+++ JSONParsingExample/app/src/main/java/iss/workshop/jsonparsingexample/BarChartActivity.java
@@ -3,6 +3,11 @@ package iss.workshop.jsonparsingexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -22,8 +27,11 @@ public class BarChartActivity extends AppCompatActivity implements GetRawData.On
 
     ArrayList<BarEntry> entries = null;
     ArrayList<String> labels = null;
+    ArrayList<String> stationeries = null;
     public String mURL;
     BarChart barChart;
+    Spinner spinnerStationary;
+    JSONArray itemsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,7 @@ public class BarChartActivity extends AppCompatActivity implements GetRawData.On
         setContentView(R.layout.activity_bar_chart);
 
         barChart = (BarChart) findViewById(R.id.barchart);
-        mURL = "http://192.168.68.110/store/storeclerkdisbursementdetailslistapi" ;
+        mURL = "http://169.254.105.22:8080/store/storeclerkdisbursementdetailslistapi" ;
 
         entries = new ArrayList<>();
         /*entries.add(new BarEntry(8f, 0));
@@ -56,7 +64,49 @@ public class BarChartActivity extends AppCompatActivity implements GetRawData.On
         barChart.setDescription("Total Orders by Department");  // set the description
         bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
 
-
+        stationeries = new ArrayList<>();
+        itemsArray = new JSONArray();
+        spinnerStationary = findViewById(R.id.spinner1);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, stationeries);
+        spinnerStationary.setAdapter(spinnerAdapter);
+//        spinnerStationary.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int o, long l) {
+//
+//                String item = spinnerStationary.getSelectedItem().toString();
+//                Toast.makeText(view.getContext(),"Test",Toast.LENGTH_LONG).show();
+//                try {
+//                    //base on dept name group quantity disbursed
+//                    for (int i = 0; i < labels.size(); i++) {
+//                        String selectedDepartment = labels.get(i);
+//                        for (int j = 0; j < itemsArray.length(); j++) {
+//                            JSONObject jsonDisbursementDetail = itemsArray.getJSONObject(j);
+//                            String deptName = jsonDisbursementDetail.getString("DeptName");
+//                            String stationary = jsonDisbursementDetail.getString("StationeryName");
+//                            if (selectedDepartment.equals(deptName) && item.equals(stationary)) {
+//                                BarEntry entry = entries.get(i);
+//                                entry.setVal(entry.getVal() + jsonDisbursementDetail.getInt("Qty"));
+//                            }
+//                        }
+//                    }
+//                } catch(Exception e){
+//                    e.printStackTrace();
+//                }
+//
+//                BarDataSet bardataset = new BarDataSet(entries, "Cells");
+//                BarData bardata = new BarData(labels, bardataset);
+//                barChart.setData(bardata);
+//                barChart.animateY(1250);
+//                barChart.notifyDataSetChanged();
+//                barChart.invalidate();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
     }
 
     @Override
@@ -75,7 +125,7 @@ public class BarChartActivity extends AppCompatActivity implements GetRawData.On
                 JSONObject jsonData = new JSONObject(data);
 
 
-                JSONArray itemsArray = jsonData.getJSONArray("requisitions");
+                itemsArray = jsonData.getJSONArray("requisitions");
 
                 //get dept name
                 int barEntryIndex = 0;
@@ -86,6 +136,11 @@ public class BarChartActivity extends AppCompatActivity implements GetRawData.On
                         labels.add(deptName);
                         entries.add(new BarEntry(0f,barEntryIndex));
                         barEntryIndex++;
+                    }
+
+                    String stationeryName = jsonDisbursementDetail.getString("StationeryName");
+                   if(!stationeries.contains(stationeryName)){
+                       stationeries.add(stationeryName);
                     }
                 }
 
